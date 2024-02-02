8e9787c3b45ba15709b45c59b7243ec6424245f6
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-02 14:59:14 +0700

    done with excercise 1

--- Excercise1.js
+++ Excercise1.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+function calculateDiscount(price, category) {
+        let discount
+        if (category === 'electronics') {
+            discount = price * 0.10
+            console.log(discount)
+        } else {
+            discount = price * 0.05
+            console.log(discount)
+        }
+        return discount
+    }
+
