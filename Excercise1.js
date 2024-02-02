b59c069d6c39f0d573c65c0b07daa89d04689b88
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-02 16:08:21 +0700

    scope problem

--- Excercise1.js
+++ Excercise1.js
@@ -1,12 +1,7 @@
-function calculateDiscount(price, category) {
-        let discount
-        if (category === 'electronics') {
-            discount = price * 0.10
-            console.log(discount)
-        } else {
-            discount = price * 0.05
-            console.log(discount)
-        }
-        return discount
+function displayMessage(x) {
+    let message = 'Hello'
+    if (x) {
+        message = 'Hello, this is a scope problem!'
     }
-
+    return message;
+}
