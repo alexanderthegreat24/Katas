e986049ac0bb9ca583c5d399ac47963b06eb1435
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-02 16:23:00 +0700

    Excercise 2A( Parameters w/ conditionalas

--- Excercise2A.js
+++ Excercise2A.js
@@ -1,5 +1,9 @@
-function multiplyNumbers(a, b) {
-        return a * b
-    }
-console.log(multiplyNumbers(5, 3));
 
+function checkLottery(ticketNumber, winningTicket) {
+    var message = 'Winner';
+    if (ticketNumber === winningTicket) {
+        return message
+    } else {
+        return 'Loser';
+    } 
+};
